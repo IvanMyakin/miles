8768e1d10a1f3498a86a7a85bb6788819234533a
Ivan Myakinkin <myakinkin.iwan@yandex.ru>
2023-01-06 21:41:30 +0700

    Charge miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 8500;
+        int bonusMiles;
+        bonusMiles = 8500 / 20;
+        System.out.println("Вам начислено" + " " + bonusMiles + " " + "бонусных милей");
+    }
+}
